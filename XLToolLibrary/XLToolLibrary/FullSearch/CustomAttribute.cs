52a8399a50ab0a124333ca36658ca9b5815d32f1
LxzLLL <minepop09@163.com>
2016-03-31 18:06:29 +0800

    add customattribute.cs

---
+++ XLToolLibrary/XLToolLibrary/FullSearch/CustomAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XLToolLibrary.Utilities.FullSearch
+{
+    /// <summary>
+    /// 作者：Arvin
+    /// 日期：2015/9/11 10:39:05
+    /// 描述：自定义的特性，用于反射，定义对象在分词时的特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple=true)]
+    public class SegmentAttribute:Attribute
+    {
+        //是否分词
+        private bool _isSegment;
+        //特性信息
+        private string _message;
+
+        public SegmentAttribute( bool blnIsSegment )
+        {
+            this._isSegment = blnIsSegment;
+        }
+
+        //是否分词
+        public bool IsSegment
+        {
+            get { return this._isSegment; }
+        }
+
+        //特性信息
+        public string Message
+        {
+            get{return this._message;}
+            set { this._message = value; }
+        }
+    }
+}
